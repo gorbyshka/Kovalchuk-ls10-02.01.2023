b2e8302462f602b29bf1e3c305bc1193b36d6db2
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2023-01-13 13:59:59 +0200

    sample

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const num = prompt('Введите число');
-const degree = prompt('Введите степень');
+const num = Number(prompt('Введите число'));
+const degree = Number(prompt('Введите степень'));
 
 function getExponentiation(num, degree) {
-    return (degree == 1) ? num : (num * getExponentiation(num, degree - 1));
+    return (degree === 1) ? num : (num * getExponentiation(num, degree - 1));
 }
 
 alert(getExponentiation(num, degree));
